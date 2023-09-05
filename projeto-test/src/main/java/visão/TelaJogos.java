bc26efe7d804ff3c775c1c1392d05a4a165effd5
Lemes <lucas.al2003@aluno.ifsc.edu.br>
2023-09-05 08:36:32 -0300

    design

--- projeto-test/src/main/java/visão/TelaJogos.java
+++ projeto-test/src/main/java/visão/TelaJogos.java
@@ -9,10 +9,20 @@ import javax.swing.border.EmptyBorder;
 
 import Controle.jogoDAO;
 import modelo.jogo;
+import java.awt.Color;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaJogos extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textField;
+	private JTextField textField_1;
 
 	/**
 	 * Launch the application.
@@ -47,8 +57,59 @@ public class TelaJogos extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
-	}
+		contentPane.setLayout(null);
 		
-
-
+		JPanel panel = new JPanel();
+		panel.setBackground(new Color(128, 128, 128));
+		panel.setBounds(329, 47, 339, 49);
+		contentPane.add(panel);
+		panel.setLayout(null);
+		
+		JLabel lblNewLabel = new JLabel("ID jogo");
+		lblNewLabel.setBounds(10, 11, 65, 27);
+		panel.add(lblNewLabel);
+		
+		textField = new JTextField();
+		textField.setBounds(85, 11, 244, 27);
+		panel.add(textField);
+		textField.setColumns(10);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBackground(Color.GRAY);
+		panel_1.setBounds(1040, 47, 339, 49);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		textField_1.setBounds(85, 11, 244, 27);
+		panel_1.add(textField_1);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nome_jogo");
+		lblNewLabel_1.setBounds(10, 11, 65, 27);
+		panel_1.add(lblNewLabel_1);
+		
+		JPanel panel_2 = new JPanel();
+		panel_2.setBackground(new Color(255, 255, 255));
+		panel_2.setBounds(327, 155, 1537, 854);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		JButton bntCadastrar = new JButton("Cadastrar");
+		bntCadastrar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+			}
+		});
+		bntCadastrar.setBounds(37, 155, 243, 49);
+		contentPane.add(bntCadastrar);
+		
+		JButton bntAlterar = new JButton("Alterar");
+		bntAlterar.setBounds(37, 233, 243, 49);
+		contentPane.add(bntAlterar);
+		
+		JButton bntExcluir = new JButton("Excluir");
+		bntExcluir.setBounds(37, 302, 243, 49);
+		contentPane.add(bntExcluir);
+	}
 }
