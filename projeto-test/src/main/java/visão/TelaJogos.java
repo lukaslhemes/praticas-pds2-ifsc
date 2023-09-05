1ce013ba78b27cf2fd4be1b37864d7a0d93c6419
Lemes <lucas.al2003@aluno.ifsc.edu.br>
2023-09-05 08:35:35 -0300

    Tela jogos

--- projeto-test/src/main/java/visão/TelaJogos.java
+++ projeto-test/src/main/java/visão/TelaJogos.java
@@ -1,14 +1,23 @@
 package visão;
 
+import java.awt.EventQueue;
 import java.util.ArrayList;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
 
 import Controle.jogoDAO;
 import modelo.jogo;
 
-public class TelaJogos {
+public class TelaJogos extends JFrame {
 
-		
-	public static void main(String[] args ) {
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
 		
 		jogoDAO dao = new jogoDAO();
 		ArrayList <jogo> Jogo = dao.listar();	
@@ -16,7 +25,30 @@ public class TelaJogos {
 			for (jogo jogo: Jogo) {
 				
 			}
-		}
-	
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					TelaJogos frame = new TelaJogos();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public TelaJogos() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 1982, 1100);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+	}
+		
+
 
 }
